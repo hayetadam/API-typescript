1eebfdb14fdfe8345c3f1495e614ac2265983eae
hayet <habethayet@gmail.com>
2017-09-08 11:16:27 +0200

    ajouter et supprimer  un user

--- dist/usersList.js
+++ dist/usersList.js
@@ -1,28 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var user_1 = require("./user");
 var UserList = /** @class */ (function () {
+    //on fait un constrocteur ou bien il faut juste définir users: User[] = []; c'est un tableau vide
     function UserList() {
+        this.users = [];
     }
-    //ajouter un utilisateur a un tableau utilsateurs
     UserList.prototype.add = function (u) {
         this.users.push(u);
     };
-    //suprimer utilisateur de tableau 
-    UserList.prototype.del = function (u) {
-        // on patcourt le tableau 
+    UserList.prototype.del = function (name) {
         for (var i = 0; i < this.users.length; i++) {
-            //si le nom correspant a l'utilisteur on le suprime avec la function splice
-            if (this.users[i].name == u.name) {
+            if (this.users[i].name == name) {
                 this.users.splice(i, 1);
             }
         }
     };
-    // autentifier un utilisateur 
     UserList.prototype.auth = function (name, password) {
         for (var _i = 0, _a = this.users; _i < _a.length; _i++) {
-            var u = _a[_i];
-            if (u.name == name && u.password == password) {
-                return u;
+            var u_1 = _a[_i];
+            if (u_1.name == name && u_1.password == password) {
+                return u_1;
             }
         }
         return null;
@@ -30,4 +28,12 @@ var UserList = /** @class */ (function () {
     return UserList;
 }());
 exports.UserList = UserList;
+var list = new UserList();
+list.add(new user_1.User("toto", "toto"));
+list.add(new user_1.User("titi", "titi"));
+console.log(list);
+list.del("toto");
+console.log(list);
+var u = list.auth("titi", "titi");
+console.log(u);
 //# sourceMappingURL=usersList.js.map
--- usersList.ts
+++ usersList.ts
@@ -1,28 +1,40 @@
-import{User} from'./user';
+import {User} from './user';
 
 export class UserList {
     users: User[];
-    //ajouter un utilisateur a un tableau utilsateurs
-    add(u: User){
+    //on fait un constrocteur ou bien il faut juste définir users: User[] = []; c'est un tableau vide
+
+    constructor() {
+        this.users = [];
+    }
+    
+    add(u: User) {
         this.users.push(u);
     }
-    //suprimer utilisateur de tableau 
-    del(u: User){
-        // on patcourt le tableau 
-     for(let i=0; i < this.users.length; i++){
-         //si le nom correspant a l'utilisteur on le suprime avec la function splice
-         if(this.users[i].name == u.name){
-             this.users.splice(i, 1);
-         }
-     }
+
+    del(name: string) {
+        for (let i = 0; i < this.users.length; i++) {
+            if (this.users[i].name == name) {
+                this.users.splice(i, 1);
+            }
+        }
     }
-    // autentifier un utilisateur 
-    auth(name: string, password: string): User{
-        for(let u of this.users){
-            if(u.name == name && u.password == password){
+
+    auth(name: string, password: string): User {
+        for (let u of this.users) {
+            if (u.name == name && u.password == password) {
                 return u;
             }
         }
         return null;
     }
 }
+
+let list = new UserList();
+list.add(new User("toto", "toto"));
+list.add(new User("titi", "titi"));
+console.log(list);
+list.del("toto");
+console.log(list);
+let u = list.auth("titi", "titi");
+console.log(u);
